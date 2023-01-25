b3ccd3091f0298df91006e2e16ab286aab284136
kew <kewber0@gmail.com>
2023-01-24 19:58:22 -0600

    tax

--- src/main/kotlin/game/monopoly_game/data/TileEffects.kt
+++ src/main/kotlin/game/monopoly_game/data/TileEffects.kt
@@ -2,13 +2,16 @@ package game.monopoly_game.data
 
 import game.monopoly_game.MonopolyGame
 import game.monopoly_game.MonopolyPlayer
+import kotlin.math.min
 
 enum class TileEffects(val effect: (p: MonopolyPlayer, gs: MonopolyGame) -> Unit = { _, _ -> }) {
     GO,  // Go tile effect already handled by MonopolyGame.moveTo()
     PARKING,  // Parking does nothing
     CHEST,
     CHANCE,
-    TAX,
+    TAX({ p: MonopolyPlayer, gs: MonopolyGame ->
+        p.deductMoney(min(p.money / 10, 200))
+    }),
     GO_TO_JAIL({ p: MonopolyPlayer, gs: MonopolyGame ->
         gs.sendToJail(p)
         gs.incrementPly()
